bfb72e7684daf483bc3002b5cb84a1bb44ce87db
Predrag Radak <radakpredrag10@gmail.com>
2023-05-27 07:54:24 +0200

    PersonalTasks

--- Project_PolyInf_2023/mongoDB_component/mongoDB_controller.py
+++ Project_PolyInf_2023/mongoDB_component/mongoDB_controller.py
@@ -8,13 +8,13 @@ class MongoController:
         self.for_database_type = "mongo"
         self.mongo_tab_model = MongoTabModel()
         self.mongo_utils = MongoUtils()
-
+    
     def load_table_data(self, database_name, table_name, dataTableWidget):
         self.mongo_tab_model.load_table_data(database_name, table_name, dataTableWidget)
-
+    
     def delete_row(self, database_name, table_name, dataTableWidget):
         print("Uraditi brisanje")
-
+    
     def insert(self, database_name, table_name, dataTableWidget, statusBar):
         print("Uraditi insert")
 
